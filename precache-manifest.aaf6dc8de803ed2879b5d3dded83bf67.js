b29fea967e2162cd479257a4d6f0d4b68b36a7fb
Edi Selimi <selimi.edi.1991@gmail.com>
2020-10-08 20:43:40 +0200

    Updates

---
+++ precache-manifest.aaf6dc8de803ed2879b5d3dded83bf67.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd4dce4d65fa4ef5bff17d6979742b3",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "394fa4997834683ade0e",
+    "url": "/ReactPortfolio/static/css/main.48332ffe.chunk.css"
+  },
+  {
+    "revision": "48916561c09badab542a",
+    "url": "/ReactPortfolio/static/js/2.dbe6655b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.dbe6655b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394fa4997834683ade0e",
+    "url": "/ReactPortfolio/static/js/main.058d3fe6.chunk.js"
+  },
+  {
+    "revision": "245e88873ef0a2539a5e",
+    "url": "/ReactPortfolio/static/js/runtime-main.ebaa2495.js"
+  },
+  {
+    "revision": "8e08d3d378c8561503f466b270205399",
+    "url": "/ReactPortfolio/static/media/Blog-Articulo.8e08d3d3.png"
+  },
+  {
+    "revision": "31d336fdf4a82b9f7316b634f20f946b",
+    "url": "/ReactPortfolio/static/media/Blog-Blog.31d336fd.png"
+  },
+  {
+    "revision": "86851376baac15202b87812ecd5f3ea3",
+    "url": "/ReactPortfolio/static/media/Blog-CrearArticulo.86851376.png"
+  },
+  {
+    "revision": "e22bfde3bb78c596517879ec51ea8ff5",
+    "url": "/ReactPortfolio/static/media/Blog-Formulario.e22bfde3.png"
+  },
+  {
+    "revision": "a57003480bf18462555ba3f833c635c9",
+    "url": "/ReactPortfolio/static/media/Blog-Inicio.a5700348.png"
+  },
+  {
+    "revision": "6f59ca2277d7421be2c4fcb1911db3b1",
+    "url": "/ReactPortfolio/static/media/CanvasTime.6f59ca22.png"
+  },
+  {
+    "revision": "65bdb7463202e57bf0bb96406bd6c6ea",
+    "url": "/ReactPortfolio/static/media/CodePlayer.65bdb746.png"
+  },
+  {
+    "revision": "2c7238b45090a30634a4cdaafe35ebbe",
+    "url": "/ReactPortfolio/static/media/Covid.2c7238b4.png"
+  },
+  {
+    "revision": "0066d07098ccd76dfd50db987bf7e6d4",
+    "url": "/ReactPortfolio/static/media/Devices.0066d070.png"
+  },
+  {
+    "revision": "4a6f63e5674ae57d1e9e2753f1cdd8f9",
+    "url": "/ReactPortfolio/static/media/DoveSiamo.4a6f63e5.png"
+  },
+  {
+    "revision": "a50c7529b6d2a7d3326653f3d7a3caea",
+    "url": "/ReactPortfolio/static/media/Home.a50c7529.png"
+  },
+  {
+    "revision": "8a1a4eaa31d508e226944448a6c2617c",
+    "url": "/ReactPortfolio/static/media/Home1.8a1a4eaa.png"
+  },
+  {
+    "revision": "3f2db41bb084ff04877589f0f2e377a9",
+    "url": "/ReactPortfolio/static/media/Home2.3f2db41b.png"
+  },
+  {
+    "revision": "a8e3a076fda5312e878c6ef9ebbab997",
+    "url": "/ReactPortfolio/static/media/OldStandardTT-Regular.a8e3a076.ttf"
+  },
+  {
+    "revision": "059440578d210177eda010943b24d74e",
+    "url": "/ReactPortfolio/static/media/Orario.05944057.png"
+  },
+  {
+    "revision": "a07a2bede9c045633e546b49a13e886a",
+    "url": "/ReactPortfolio/static/media/Photogallery.a07a2bed.png"
+  },
+  {
+    "revision": "f72e273f42680b48ffc9153bc2c69654",
+    "url": "/ReactPortfolio/static/media/PlanChoosed.f72e273f.png"
+  },
+  {
+    "revision": "07380ecdc9d2f12b011ab0e6ee259fbe",
+    "url": "/ReactPortfolio/static/media/Plans.07380ecd.png"
+  },
+  {
+    "revision": "3ce34f965bc1e29bd59fadfb88fe982a",
+    "url": "/ReactPortfolio/static/media/Portfolio.3ce34f96.png"
+  },
+  {
+    "revision": "b603cafcbf2932c10215f831b4af8e90",
+    "url": "/ReactPortfolio/static/media/Prezzi.b603cafc.png"
+  },
+  {
+    "revision": "d1444bd59dd8d244d191ec660c261585",
+    "url": "/ReactPortfolio/static/media/Signin.d1444bd5.png"
+  },
+  {
+    "revision": "a01f1d7e13b67552cf53fb8af64f98cc",
+    "url": "/ReactPortfolio/static/media/Todoapp.a01f1d7e.png"
+  },
+  {
+    "revision": "fc0e4e53644b488b966c9c31b4324a85",
+    "url": "/ReactPortfolio/static/media/Videogame.fc0e4e53.png"
+  },
+  {
+    "revision": "6fc37cdcf2242327cb01742a87384867",
+    "url": "/ReactPortfolio/static/media/bg-image.6fc37cdc.png"
+  },
+  {
+    "revision": "4eb1c63bf4cba0105c217a687195fdd1",
+    "url": "/ReactPortfolio/static/media/calculator.4eb1c63b.png"
+  },
+  {
+    "revision": "b46afb5818bd081a5538bb2876813e9d",
+    "url": "/ReactPortfolio/static/media/covid2.b46afb58.png"
+  },
+  {
+    "revision": "d1ecc06bf680854468934a68e82ca06f",
+    "url": "/ReactPortfolio/static/media/covid3.d1ecc06b.png"
+  },
+  {
+    "revision": "f3747344f10f4840baeced289a0b55f5",
+    "url": "/ReactPortfolio/static/media/cravatta2.f3747344.jpg"
+  },
+  {
+    "revision": "61e5947047094478ccc5d105ec0fb890",
+    "url": "/ReactPortfolio/static/media/edi.61e59470.jpeg"
+  },
+  {
+    "revision": "de5df75c93b66c822b4c7ea809f8dcdd",
+    "url": "/ReactPortfolio/static/media/heart.de5df75c.jpg"
+  },
+  {
+    "revision": "1e181692362fe42fffd254ca93dd00a1",
+    "url": "/ReactPortfolio/static/media/item1.1e181692.jpg"
+  },
+  {
+    "revision": "39c0ffbd439b7070fc10a8f9c9fb81c3",
+    "url": "/ReactPortfolio/static/media/item10.39c0ffbd.jpg"
+  },
+  {
+    "revision": "512a44a790e5b30e77b05e754929858c",
+    "url": "/ReactPortfolio/static/media/item11.512a44a7.jpg"
+  },
+  {
+    "revision": "fda2dc8b72a27155c5433d43f184d8d1",
+    "url": "/ReactPortfolio/static/media/item12.fda2dc8b.jpg"
+  },
+  {
+    "revision": "a7cf45cc78b11da0ebb9484b49865afd",
+    "url": "/ReactPortfolio/static/media/item2.a7cf45cc.jpg"
+  },
+  {
+    "revision": "3128194fa413ef58314a054912f1a50f",
+    "url": "/ReactPortfolio/static/media/item3.3128194f.jpg"
+  },
+  {
+    "revision": "de2c59c29833568d3c60f0833fab9bbe",
+    "url": "/ReactPortfolio/static/media/item4.de2c59c2.jpg"
+  },
+  {
+    "revision": "3bcb9096449fa5ee0ca1882e74106816",
+    "url": "/ReactPortfolio/static/media/item5.3bcb9096.jpg"
+  },
+  {
+    "revision": "62988967950a3656f0627195df3eb548",
+    "url": "/ReactPortfolio/static/media/item6.62988967.jpg"
+  },
+  {
+    "revision": "51a24fe10c7a3a979897c37f419c65af",
+    "url": "/ReactPortfolio/static/media/item7.51a24fe1.jpg"
+  },
+  {
+    "revision": "2180aa6cb4d447629a9cd855de740500",
+    "url": "/ReactPortfolio/static/media/item8.2180aa6c.jpg"
+  },
+  {
+    "revision": "bc962ecc602f6a9d30e1d0a11cbe1852",
+    "url": "/ReactPortfolio/static/media/item9.bc962ecc.jpg"
+  },
+  {
+    "revision": "997ad6fe1acddce2512ddc1b3f7e60da",
+    "url": "/ReactPortfolio/static/media/logoEdi.997ad6fe.jpg"
+  },
+  {
+    "revision": "e09106e53b99a0895f50d42a440f742a",
+    "url": "/ReactPortfolio/static/media/meta-chart.e09106e5.png"
+  },
+  {
+    "revision": "0eeb173533e29faea1b801fff1cae4ce",
+    "url": "/ReactPortfolio/static/media/musicAppItem.0eeb1735.jpg"
+  },
+  {
+    "revision": "a502d7c3bafc4960bbae190497fa437a",
+    "url": "/ReactPortfolio/static/media/recipeAppItem.a502d7c3.png"
+  },
+  {
+    "revision": "ccc9ef0c156e2277e0ed417265653ff4",
+    "url": "/ReactPortfolio/static/media/recipeAppItem2.ccc9ef0c.png"
+  },
+  {
+    "revision": "1cc251a645af875e7b24fd9e80bae9db",
+    "url": "/ReactPortfolio/static/media/recipeAppItem3.1cc251a6.png"
+  },
+  {
+    "revision": "0eec060cdb89631c3cbca647e8773ff6",
+    "url": "/ReactPortfolio/static/media/softskills.0eec060c.png"
+  }
+]);
